d436fa69f58d6c33156e31e9b9abd80a3a1943a1
nathanwang26 <nathan.wang@scsstudent.org>
2025-11-04 22:04:09 -0800

    Revert "feat: Complete Videos 08 & 09"
    This reverts commit 940f5c021375a9f69855e0b0922a163468678862.

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,11 +1,8 @@
-import { Text, StyleSheet, View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
 import { PlantlyButton } from "@/components/PlantlyButton";
-import { LinearGradient } from "expo-linear-gradient";
-import { StatusBar } from "expo-status-bar";
-import { PlantlyImage } from "@/components/PlantlyImage";
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -17,42 +14,17 @@ export default function OnboardingScreen() {
   };
 
   return (
-    <LinearGradient
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 1 }}
-      colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
-      style={styles.container}
-    >
-      <StatusBar style="light" />
-      <View>
-        <Text style={styles.heading}>Plantly</Text>
-        <Text style={styles.tagline}>
-          Keep your plants healthy and hydrated
-        </Text>
-      </View>
-      <PlantlyImage />
+    <View style={styles.container}>
       <PlantlyButton title="Let me in!" onPress={handlePress} />
-    </LinearGradient>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-evenly",
+    justifyContent: "center",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
-  },
-  heading: {
-    fontSize: 42,
-    color: theme.colorWhite,
-    fontWeight: "bold",
-    marginBottom: 12,
-    textAlign: "center",
-  },
-  tagline: {
-    fontSize: 24,
-    color: theme.colorWhite,
-    textAlign: "center",
   },
 });
