6398cb4a729f198f440ab7bd1696c34524a6f723
nathanwang26 <nathan.wang@scsstudent.org>
2025-11-19 00:57:00 -0800

    feat: Complete Video 21

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,10 @@
-import { Redirect, SplashScreen, Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import Entypo from "@expo/vector-icons/Entypo";
 import Feather from "@expo/vector-icons/Feather";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 
 export default function Layout() {
-  SplashScreen.hideAsync();
   const hasFinishedOnboarding = useUserStore(
     (state) => state.hasFinishedOnboarding
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,23 @@
-import { SplashScreen, Stack } from "expo-router";
-
-SplashScreen.preventAutoHideAsync();
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import * as QuickActions from "expo-quick-actions";
+import { Platform } from "react-native";
+import { useQuickActionRouting } from "expo-quick-actions/router";
 
 export default function Layout() {
+  useQuickActionRouting();
+
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add a plant",
+        icon: Platform.OS === "ios" ? "symbol:leaf" : "leaf",
+        id: "0",
+        params: { href: "/new" },
+      },
+    ]);
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen
